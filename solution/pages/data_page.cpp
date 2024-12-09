676592b889cbe684d83a1146c0dc02f41527c7d5
Connor George <connorgeorgeem@aol.com>
2024-12-09 18:15:45 +0000

    added filter to data page

--- solution/pages/data_page.cpp
+++ solution/pages/data_page.cpp
@@ -3,17 +3,28 @@
 DataPage::DataPage(WaterSampleTableModel* model, QWidget* parent): Page(model, parent) {
     addHeader2Text("Data Page");
 
+    // add filter input
+    filterInput = new QLineEdit();
+    filterInput->setPlaceholderText("Filter...");
+    connect(filterInput, SIGNAL(textChanged(const QString&)), this, SLOT(applyFilter(const QString&)));
+    addWidget(filterInput);
+
+    // create filter proxy model
+    filterProxyModel = new QSortFilterProxyModel(this);
+    filterProxyModel->setSourceModel(model);
+    filterProxyModel->setFilterKeyColumn(-1);
+    filterProxyModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+
     // add table
     QTableView* table = new QTableView(this);
-
-    QSortFilterProxyModel* proxyModel = new QSortFilterProxyModel(this);
-    proxyModel->setSourceModel(model);
-    proxyModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
-
-    table->setModel(proxyModel);
+    table->setModel(filterProxyModel);
 
     QFont tableFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
     table->setFont(tableFont);
 
     addWidget(table);
+}
+
+void DataPage::applyFilter(const QString& text) {
+    filterProxyModel->setFilterFixedString(text);
 }
--- solution/pages/data_page.hpp
+++ solution/pages/data_page.hpp
@@ -5,10 +5,20 @@
 #include <QSortFilterProxyModel>
 #include <QFontDatabase>
 #include <QWidget>
+#include <QLineEdit>
 #include "page.hpp"
 #include "../table_model.hpp"
 
 class DataPage: public Page {
+    Q_OBJECT
+
     public:
         DataPage(WaterSampleTableModel* model, QWidget* parent = nullptr);
+    
+    private:
+        QLineEdit* filterInput;
+        QSortFilterProxyModel* filterProxyModel;
+    
+    private slots:
+        void applyFilter(const QString& text);
 };
