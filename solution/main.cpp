4eb20b00298a1f0787704e3668d8008c67381883
Lisa Zhao <84100023+lisazhao30@users.noreply.github.com>
2024-12-17 23:08:41 +0000

    mac build fix

--- solution/main.cpp
+++ solution/main.cpp
@@ -3,7 +3,11 @@
 
 int main(int argc, char* argv[])
 {
+#if defined(Q_OS_WIN)
   qputenv("QT_QPA_PLATFORM", "windows:darkmode=0");
+#elif defined(Q_OS_MAC)
+  qputenv("QT_QPA_PLATFORM", "cocoa");
+#endif
   QApplication app(argc, argv);
 
   WaterSampleWindow window;
