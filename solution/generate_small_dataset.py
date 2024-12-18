a7c4af2af644fa06be190e6ddf3cfec5d48cb4d2
Connor George <connorgeorgeem@aol.com>
2024-12-18 18:42:27 +0000

    larger dataset, db generation python script uses random rows

--- solution/generate_small_dataset.py
+++ solution/generate_small_dataset.py
@@ -6,12 +6,20 @@
 # https://github.com/zblesk/csv-to-sqlite
 
 import csv
+import random
+
+NUM_OF_ROWS = 5000
 
 rows = []
 with open("data/2024.csv", "r") as f:
     reader = csv.reader(f)
-    for _ in range(500):
-        rows.append(next(reader))
+    all_rows = list(reader)
+
+row_count = len(all_rows)
+rows_to_select = random.sample(range(1, row_count-1), NUM_OF_ROWS)
+rows.append(all_rows[0]) # Header
+for i in rows_to_select:
+    rows.append(all_rows[i])
 
 with open("data/2024_small.csv", "w") as f:
     writer = csv.writer(f, lineterminator="\n")
